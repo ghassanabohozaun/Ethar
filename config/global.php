2ff9c2d00644d07f40fdf41dbb4737dff2b55944
ahmedZread123 <yahmed9013@gmail.com>
2023-01-11 14:15:46 +0200

    projects

--- config/global.php
+++ config/global.php
@@ -9,14 +9,9 @@ return [
         'roles'          => 'app.roles',
         'users'          => 'app.users',
         'support-center' => 'app.support-center',
-<<<<<<< HEAD
         'articles'       => 'app.articles',
         'landing-page'   =>'app.landing-page',
         'projects'       => 'app.projects'
-=======
-        'articles' => 'app.articles',
-        'landing-page'=>'app.landing-page',
-        'projects'=>'app.projects',
->>>>>>> a41705afa03b13a9b0dacf619b237e760cb018cb
+
     ]
 ];
