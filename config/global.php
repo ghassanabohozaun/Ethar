d4bdd054d901ae032a194a83695e2d5f1564ce1e
ahmedZread123 <yahmed9013@gmail.com>
2023-01-14 10:56:35 +0200

    report

--- config/global.php
+++ config/global.php
@@ -9,14 +9,7 @@ return [
         'roles' => 'app.roles',
         'users' => 'app.users',
         'support-center' => 'app.support-center',
-<<<<<<< HEAD
-        'articles'       => 'app.articles',
-        'landing-page'   =>'app.landing-page',
-        'projects'       => 'app.projects',
-        'testimonials'   => 'app.testimonials',
-        'publications'   => 'app.publications',
-        'reports'        => 'app.reports',
-=======
+
         'articles' => 'app.articles',
         'landing-page' => 'app.landing-page',
         'projects' => 'app.projects',
@@ -26,6 +19,6 @@ return [
         'photos' => 'app.photos',
         'yearly-reports' => 'app.yearly-reports',
 
->>>>>>> dee12d86212397945f3c4cc440a05cad09be261a
+
     ]
 ];
