a7c0f754881747a880906faf8dbf79eafb3fa675
ahmedZread123 <yahmed9013@gmail.com>
2023-01-17 12:53:06 +0200

    report

--- resources/lang/ar/reports.php
+++ resources/lang/ar/reports.php
@@ -12,6 +12,7 @@ return [
 
     'financial'=>'تقرير مالي',
     'administrative'=>'تقرير اداري',
+    'has_delete'  => 'هذا النوع من التقرير في هذه السنة موجود  في سلة المحذوفات ',
 
     'this_type_of_report_has_been_added_for_this_year'=>'هذا النوع من التقرير تم اضافه لهذه  السنة مسبقا',
 
