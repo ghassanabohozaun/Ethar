80382263cac15875e988b4d1a783a52a6bd10183
ghassan <ghassan@digitalorder.net>
2023-02-22 15:56:26 +0200

    env

---
+++ app/Http/Controllers/Site/SiteController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers\Site;
+
+use App\File;
+use App\Http\Controllers\Controller;
+use App\Models\Slider;
+use App\Traits\GeneralTrait;
+use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
+
+class SiteController extends Controller
+{
+    use GeneralTrait;
+
+    //////////////////////////////////////////////////////////////////////////////////////////
+    /////reloadCaptcha
+    public function reloadCaptcha()
+    {
+        return response()->json(['captcha' => captcha_img()]);
+    }
+    //////////////////////////////////////////////////////////////////////////////////////////
+    ///index
+    public function index()
+    {
+        if (Lang() == 'ar') {
+            $title = setting()->site_name_ar;
+        } else {
+            $title = setting()->site_name_en;
+        }
+        ////////////////////////////////////////////////////////////////////////////////////////////////
+        /// Arabic
+        if (LaravelLocalization::getCurrentLocale() == 'ar') {
+            /// Slider
+            $sliders = Slider::orderByDesc('id')->where('status', 'on')
+                ->where(function ($q) {
+                    $q->where('language', 'ar')
+                        ->orWhere('language', 'ar_en');
+                })->take(2)->get();
+        } else {
+            /// Slider
+            $sliders = Slider::orderByDesc('id')->where('status', 'on')
+                ->where(function ($q) {
+                    $q->where('language', 'ar_en');
+                })->take(2)->get();
+        }
+
+
+
+        return view('site.index', compact('title', 'sliders'));
+    }
+
+}
