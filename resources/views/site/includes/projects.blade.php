c8024f9a94d2b3143551eab42c2245bf1af42a5b
ghassan <ghassan@digitalorder.net>
2023-02-23 14:02:18 +0200

    test

--- resources/views/site/includes/projects.blade.php
+++ resources/views/site/includes/projects.blade.php
@@ -3,7 +3,7 @@
     <div class="auto-container">
         <div class="sec-title style-two centred">
             <span class="top-text">About Our Projects</span>
-            <h2>Current Projects</h2>
+            <h2>Our Projects</h2>
             <p>
                 Lorem Ipsum is simply dummy text of the printing and typesetting industry.
                 Lorem Ipsum is simply dummy text of the printing and typesetting industry.
@@ -16,14 +16,21 @@
                     <div class="case-block-one">
                         <div class="inner-box">
                             <figure class="image-box">
-                                <img src="{{asset('adminBoard/uploadedImages/projects/'.$currentProject->photo)}}" alt=""></figure>
+                                <img src="{{asset('adminBoard/uploadedImages/projects/'.$currentProject->photo)}}"
+                                     alt=""></figure>
                             <div class="lower-content">
                                 <div class="shape"
                                      style="background-image: url('{!! asset('/site/assets/images/shape/shape-11.png') !!}');"></div>
                                 <div class="inner">
                                     <div class="text">
-                                        <div class="category"><a href="#"># Health & Food</a></div>
-                                        <h3><a href="#">{!! Lang()=='ar'? $currentProject->title_ar:$currentProject->title_en !!}</a></h3>
+                                        <div class="category">
+                                            <a href="#">#
+                                                {!! $currentProject->type =='current'  ? __('index.current_project'):  __('index.previous_project')!!}
+                                            </a>
+                                        </div>
+                                        <h3>
+                                            <a href="#">{!! Lang()=='ar'? $currentProject->title_ar:$currentProject->title_en !!}</a>
+                                        </h3>
                                     </div>
                                 </div>
                             </div>
