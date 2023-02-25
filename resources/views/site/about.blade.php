c997313b53be23f8861cf23fb19940a3674dc18c
ghassan <ghassan@digitalorder.net>
2023-02-25 14:13:16 +0200

    updates

--- resources/views/site/about.blade.php
+++ resources/views/site/about.blade.php
@@ -97,46 +97,45 @@
                             </section>
                             <!-- about-style-three end -->
 
-                            @elseif (!isset($about))
-                                <!-- Page Title -->
-                                <section class="page-title"
-                                         style="background-image: url({!! asset('site/assets/images/background/12.jpg') !!});">
-
-                                    <div class="auto-container">
-                                        <div class="content-box">
-                                            <div class="title">
-                                                <h1>{{$about_type->{'name_'.Lang()} }}</h1>
-                                            </div>
-                                            <ul class="bread-crumb clearfix">
-                                                <li><a href="{!! route('index') !!}">Home</a></li>
-                                                {{-- <li>Pages</li> --}}
-                                                <li>{{$about_type->{'name_'.Lang()} }}</li>
-                                            </ul>
-                                        </div>
-                                    </div>
-                                </section>
-                                <!-- End Page Title -->
-                                <!-- Not Found  -->
-                                <section class="about-style-three">
-                                    <!-- Not Found  -->
-                                    <section class="about-style-three">
-                                        <div class="sec-title  text-center">
-                                            <h2>{{__('site.about_not_found')}}    </h2>
-                                        </div>
-                                    </section>
-                                </section>
-                                <!-- End  Not Found -->
-                            @endif
-
-
-                            <!-- main-footer -->
-                            @include('site.includes.footer')
-                            <!-- main-footer end -->
-
-
                         </div>
                     </div>
                 </section>
+            @elseif (!isset($about))
+                <!-- Page Title -->
+                <section class="page-title"
+                         style="background-image: url({!! asset('site/assets/images/background/12.jpg') !!});">
+
+                    <div class="auto-container">
+                        <div class="content-box">
+                            <div class="title">
+                                <h1>{{$about_type->{'name_'.Lang()} }}</h1>
+                            </div>
+                            <ul class="bread-crumb clearfix">
+                                <li><a href="{!! route('index') !!}">Home</a></li>
+                                {{-- <li>Pages</li> --}}
+                                <li>{{$about_type->{'name_'.Lang()} }}</li>
+                            </ul>
+                        </div>
+                    </div>
+                </section>
+                <!-- End Page Title -->
+                <!-- Not Found  -->
+                <section class="about-style-three">
+                    <!-- Not Found  -->
+                    <section class="about-style-three">
+                        <div class="sec-title  text-center">
+                            <h2>{{__('site.about_not_found')}}    </h2>
+                        </div>
+                    </section>
+                </section>
+                <!-- End  Not Found -->
+            @endif
+
+
+            <!-- main-footer -->
+            @include('site.includes.footer')
+            <!-- main-footer end -->
+
 
         </div>
 
