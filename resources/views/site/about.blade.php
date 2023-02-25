42f0e37209caa874be8a80b561f5ffcad6abcf7c
ghassan <ghassan@digitalorder.net>
2023-02-25 14:11:24 +0200

    updates

--- resources/views/site/about.blade.php
+++ resources/views/site/about.blade.php
@@ -15,140 +15,125 @@
 @endpush
 @section('content')
 
-<<<<<<< HEAD
-    <div class="boxed_wrapper ltr"  >
+    <div class="boxed_wrapper ltr">
 
+        <div class="boxed_wrapper {!! Lang()=='ar' ? 'rtl':'' !!}">
+            <!-- header -->
+            @include('site.includes.header')
+            <!-- header end -->
+            @if (isset($about))
 
-=======
-    <div class="boxed_wrapper {!! Lang()=='ar' ? 'rtl':'' !!}">
->>>>>>> 3eb2909414ad0b2e57a34472aed9a244b8f089bf
-        <!-- header -->
-        @include('site.includes.header')
-        <!-- header end -->
-        @if (isset($about))
-
-<<<<<<< HEAD
-            <!-- Page Title -->
-            <section class="page-title "
-                     style="background-image: url({!! asset('site/assets/images/background/12.jpg') !!});">
-                <div class="auto-container">
-                    <div class="content-box">
-                        <div class="title">
-                            <h1>{{$about->type->{'name_'.Lang()} }}</h1>
-                        </div>
-                        <ul class="bread-crumb clearfix">
-                            <li><a href="{!! route('index') !!}">Home</a></li>
-                            {{-- <li>Pages</li> --}}
-                            <li>{{$about->type->{'name_'.Lang()} }}</li>
-                        </ul>
-=======
-        <!-- Page Title -->
-        <section class="page-title"
-                 style="background-image: url({!! asset('site/assets/images/background/12.jpg') !!});">
-                
-            <div class="auto-container{!! Lang()=='ar' ? 'rtl':'' !!}">
-                <div class="content-box">
-                    <div class="title">
-                        <h1>{{$about->type->{'name_'.Lang()} }}</h1>
->>>>>>> 3eb2909414ad0b2e57a34472aed9a244b8f089bf
-                    </div>
-                </div>
-            </section>
-            <!-- End Page Title -->
-
-            <!-- about-style-three -->
-            <section class="about-style-three ">
-                <div class="auto-container">
-                    <div class="row clearfix">
-
-                        <div class="col-lg-12 col-md-12 col-sm-12 content-column">
-                            <div class="content_block_6">
-                                <div class="content-box">
-                                    <div class="sec-title">
-                                        <span class="top-text">{{$about->type->{'name_'.Lang()} }}</span>
-                                        <h2>{{$about->{'title_'.Lang()} }}</h2>
-                                    </div>
-
-                                    <div class="text">
-                                        <p>{!!$about->{'details_'.Lang()} !!}</p>
-                                    </div>
-
-                                </div>
-<<<<<<< HEAD
-=======
-
-                                <div class="text">
-                                    <p>{!!$about->{'details_'.Lang()} !!}</p>
-                                    <br/>
-                                    @isset($about->file)
-                                        
-                                    <ul class="info-box clearfix">
-                                        <li class="share">
-                                            <i class="fas fa-file-pdf">
-                                                </i> 
-                                            <a class="font-weight-bold"
-                                            href="{{asset('adminBoard\\uploadedFiles\\abouts\\'. $about->file)}}"
-                                            target="_blank">{!! __('general.download') !!}</a>
-                                           
-                                        </li>
-                                    </ul>
-                                    @endisset
-
-
-                                </div>
-
->>>>>>> 3eb2909414ad0b2e57a34472aed9a244b8f089bf
+                <!-- Page Title -->
+                <section class="page-title "
+                         style="background-image: url({!! asset('site/assets/images/background/12.jpg') !!});">
+                    <div class="auto-container">
+                        <div class="content-box">
+                            <div class="title">
+                                <h1>{{$about->type->{'name_'.Lang()} }}</h1>
                             </div>
+                            <ul class="bread-crumb clearfix">
+                                <li><a href="{!! route('index') !!}">Home</a></li>
+                                {{-- <li>Pages</li> --}}
+                                <li>{{$about->type->{'name_'.Lang()} }}</li>
+                            </ul>
+                            <!-- Page Title -->
+                            <section class="page-title"
+                                     style="background-image: url({!! asset('site/assets/images/background/12.jpg') !!});">
+
+                                <div class="auto-container{!! Lang()=='ar' ? 'rtl':'' !!}">
+                                    <div class="content-box">
+                                        <div class="title">
+                                            <h1>{{$about->type->{'name_'.Lang()} }}</h1>
+                                        </div>
+                                    </div>
+                                </div>
+                            </section>
+                            <!-- End Page Title -->
+
+                            <!-- about-style-three -->
+                            <section class="about-style-three ">
+                                <div class="auto-container">
+                                    <div class="row clearfix">
+
+                                        <div class="col-lg-12 col-md-12 col-sm-12 content-column">
+                                            <div class="content_block_6">
+                                                <div class="content-box">
+                                                    <div class="sec-title">
+                                                        <span
+                                                            class="top-text">{{$about->type->{'name_'.Lang()} }}</span>
+                                                        <h2>{{$about->{'title_'.Lang()} }}</h2>
+                                                    </div>
+
+                                                    <div class="text">
+                                                        <p>{!!$about->{'details_'.Lang()} !!}</p>
+                                                    </div>
+
+                                                </div>
+                                                <div class="text">
+                                                    <p>{!!$about->{'details_'.Lang()} !!}</p>
+                                                    <br/>
+                                                    @isset($about->file)
+
+                                                        <ul class="info-box clearfix">
+                                                            <li class="share">
+                                                                <i class="fas fa-file-pdf">
+                                                                </i>
+                                                                <a class="font-weight-bold"
+                                                                   href="{{asset('adminBoard\\uploadedFiles\\abouts\\'. $about->file)}}"
+                                                                   target="_blank">{!! __('general.download') !!}</a>
+
+                                                            </li>
+                                                        </ul>
+                                                    @endisset
+
+
+                                                </div>
+
+                                            </div>
+                                        </div>
+
+                                    </div>
+                                </div>
+                            </section>
+                            <!-- about-style-three end -->
+
+                            @elseif (!isset($about))
+                                <!-- Page Title -->
+                                <section class="page-title"
+                                         style="background-image: url({!! asset('site/assets/images/background/12.jpg') !!});">
+
+                                    <div class="auto-container">
+                                        <div class="content-box">
+                                            <div class="title">
+                                                <h1>{{$about_type->{'name_'.Lang()} }}</h1>
+                                            </div>
+                                            <ul class="bread-crumb clearfix">
+                                                <li><a href="{!! route('index') !!}">Home</a></li>
+                                                {{-- <li>Pages</li> --}}
+                                                <li>{{$about_type->{'name_'.Lang()} }}</li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </section>
+                                <!-- End Page Title -->
+                                <!-- Not Found  -->
+                                <section class="about-style-three">
+                                    <!-- Not Found  -->
+                                    <section class="about-style-three">
+                                        <div class="sec-title  text-center">
+                                            <h2>{{__('site.about_not_found')}}    </h2>
+                                        </div>
+                                    </section>
+                                </section>
+                                <!-- End  Not Found -->
+                            @endif
+
+
+                            <!-- main-footer -->
+                            @include('site.includes.footer')
+                            <!-- main-footer end -->
+
+
                         </div>
-
-                    </div>
-                </div>
-            </section>
-            <!-- about-style-three end -->
-
-        @elseif (!isset($about))
-            <!-- Page Title -->
-            <section class="page-title"
-<<<<<<< HEAD
-                     style="background-image: url({!! asset('site/assets/images/background/12.jpg') !!});">
-=======
-            style="background-image: url({!! asset('site/assets/images/background/12.jpg') !!});">
->>>>>>> 3eb2909414ad0b2e57a34472aed9a244b8f089bf
-                <div class="auto-container">
-                    <div class="content-box">
-                        <div class="title">
-                            <h1>{{$about_type->{'name_'.Lang()} }}</h1>
-                        </div>
-                        <ul class="bread-crumb clearfix">
-                            <li><a href="{!! route('index') !!}">Home</a></li>
-                            {{-- <li>Pages</li> --}}
-                            <li>{{$about_type->{'name_'.Lang()} }}</li>
-                        </ul>
-                    </div>
-                </div>
-            </section>
-            <!-- End Page Title -->
-<<<<<<< HEAD
-            <!-- Not Found  -->
-            <section class="about-style-three">
-=======
-             <!-- Not Found  -->
-             <section class="about-style-three">
->>>>>>> 3eb2909414ad0b2e57a34472aed9a244b8f089bf
-                <div class="sec-title  text-center">
-
-                    <h2>{{__('site.about_not_found')}}    </h2>
-                </div>
-            </section>
-            <!-- End  Not Found -->
-        @endif
-
-
-        <!-- main-footer -->
-        @include('site.includes.footer')
-        <!-- main-footer end -->
-
-
-    </div>
 
 @endsection
