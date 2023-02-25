ca32154909b41a17a1cc386f44ba478c68103011
ghassan <ghassan@digitalorder.net>
2023-02-25 14:06:50 +0200

    updates

--- resources/views/site/about.blade.php
+++ resources/views/site/about.blade.php
@@ -15,86 +15,84 @@
 @endpush
 @section('content')
 
-    <div class="boxed_wrapper {!! Lang()=='ar' ? 'rtl':'' !!}">
+    <div class="boxed_wrapper ltr"  >
 
 
         <!-- header -->
         @include('site.includes.header')
         <!-- header end -->
         @if (isset($about))
-        
-      
 
-        <!-- Page Title -->
-        <section class="page-title"
-                 style="background-image: url({!! asset('site/assets/images/background/12.jpg') !!});">
-            <div class="auto-container">
-                <div class="content-box">
-                    <div class="title">
-                        <h1>{{$about->type->{'name_'.Lang()} }}</h1>
+            <!-- Page Title -->
+            <section class="page-title "
+                     style="background-image: url({!! asset('site/assets/images/background/12.jpg') !!});">
+                <div class="auto-container">
+                    <div class="content-box">
+                        <div class="title">
+                            <h1>{{$about->type->{'name_'.Lang()} }}</h1>
+                        </div>
+                        <ul class="bread-crumb clearfix">
+                            <li><a href="{!! route('index') !!}">Home</a></li>
+                            {{-- <li>Pages</li> --}}
+                            <li>{{$about->type->{'name_'.Lang()} }}</li>
+                        </ul>
                     </div>
-                    <ul class="bread-crumb clearfix">
-                        <li><a href="{!! route('index') !!}">Home</a></li>
-                        {{-- <li>Pages</li> --}}
-                        <li>{{$about->type->{'name_'.Lang()} }}</li>
-                    </ul>
                 </div>
-            </div>
-        </section>
-        <!-- End Page Title -->
+            </section>
+            <!-- End Page Title -->
 
-        <!-- about-style-three -->
-        <section class="about-style-three">
-            <div class="auto-container">
-                <div class="row clearfix">
+            <!-- about-style-three -->
+            <section class="about-style-three ">
+                <div class="auto-container">
+                    <div class="row clearfix">
 
-                    <div class="col-lg-12 col-md-12 col-sm-12 content-column">
-                        <div class="content_block_6">
-                            <div class="content-box">
-                                <div class="sec-title">
-                                    <span class="top-text">{{$about->type->{'name_'.Lang()} }}</span>
-                                    <h2>{{$about->{'title_'.Lang()} }}</h2>
+                        <div class="col-lg-12 col-md-12 col-sm-12 content-column">
+                            <div class="content_block_6">
+                                <div class="content-box">
+                                    <div class="sec-title">
+                                        <span class="top-text">{{$about->type->{'name_'.Lang()} }}</span>
+                                        <h2>{{$about->{'title_'.Lang()} }}</h2>
+                                    </div>
+
+                                    <div class="text">
+                                        <p>{!!$about->{'details_'.Lang()} !!}</p>
+                                    </div>
+
                                 </div>
-
-                                <div class="text">
-                                    <p>{!!$about->{'details_'.Lang()} !!}</p>
-                                </div>
-
                             </div>
                         </div>
-                    </div>
 
+                    </div>
                 </div>
-            </div>
-        </section>
-        <!-- about-style-three end -->
+            </section>
+            <!-- about-style-three end -->
 
         @elseif (!isset($about))
-         <!-- Page Title -->
-         <section class="page-title"
-         style="background-image: url({!! asset('site/assets/images/background/12.jpg') !!});">
-            <div class="auto-container">
-                <div class="content-box">
-                    <div class="title">
-                        <h1>{{$about_type->{'name_'.Lang()} }}</h1>
+            <!-- Page Title -->
+            <section class="page-title"
+                     style="background-image: url({!! asset('site/assets/images/background/12.jpg') !!});">
+                <div class="auto-container">
+                    <div class="content-box">
+                        <div class="title">
+                            <h1>{{$about_type->{'name_'.Lang()} }}</h1>
+                        </div>
+                        <ul class="bread-crumb clearfix">
+                            <li><a href="{!! route('index') !!}">Home</a></li>
+                            {{-- <li>Pages</li> --}}
+                            <li>{{$about_type->{'name_'.Lang()} }}</li>
+                        </ul>
                     </div>
-                    <ul class="bread-crumb clearfix">
-                        <li><a href="{!! route('index') !!}">Home</a></li>
-                        {{-- <li>Pages</li> --}}
-                        <li>{{$about_type->{'name_'.Lang()} }}</li>
-                    </ul>
                 </div>
-            </div>
-        </section>
-<!-- End Page Title -->
-             <!-- Not Found  -->
-             <section class="about-style-three">
+            </section>
+            <!-- End Page Title -->
+            <!-- Not Found  -->
+            <section class="about-style-three">
                 <div class="sec-title  text-center">
-                                     
+
                     <h2>{{__('site.about_not_found')}}    </h2>
                 </div>
             </section>
-            <!-- End  Not Foun -->
+            <!-- End  Not Found -->
         @endif
 
 
