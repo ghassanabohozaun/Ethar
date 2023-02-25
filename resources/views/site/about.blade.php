7a22aa65d7e0c731d9d0702be1747643bec9fb3b
ghassan <ghassan@digitalorder.net>
2023-02-25 14:14:42 +0200

    updates

--- resources/views/site/about.blade.php
+++ resources/views/site/about.blade.php
@@ -22,10 +22,9 @@
             @include('site.includes.header')
             <!-- header end -->
             @if (isset($about))
-
                 <!-- Page Title -->
-                <section class="page-title "
-                         style="background-image: url({!! asset('site/assets/images/background/12.jpg') !!});">
+                <section class="page-title " style="background-image: url({!! asset('site/assets/images/background/12.jpg') !!});">
+
                     <div class="auto-container">
                         <div class="content-box">
                             <div class="title">
@@ -50,56 +49,11 @@
                             </section>
                             <!-- End Page Title -->
 
-                            <!-- about-style-three -->
-                            <section class="about-style-three ">
-                                <div class="auto-container">
-                                    <div class="row clearfix">
-
-                                        <div class="col-lg-12 col-md-12 col-sm-12 content-column">
-                                            <div class="content_block_6">
-                                                <div class="content-box">
-                                                    <div class="sec-title">
-                                                        <span
-                                                            class="top-text">{{$about->type->{'name_'.Lang()} }}</span>
-                                                        <h2>{{$about->{'title_'.Lang()} }}</h2>
-                                                    </div>
-
-                                                    <div class="text">
-                                                        <p>{!!$about->{'details_'.Lang()} !!}</p>
-                                                    </div>
-
-                                                </div>
-                                                <div class="text">
-                                                    <p>{!!$about->{'details_'.Lang()} !!}</p>
-                                                    <br/>
-                                                    @isset($about->file)
-
-                                                        <ul class="info-box clearfix">
-                                                            <li class="share">
-                                                                <i class="fas fa-file-pdf">
-                                                                </i>
-                                                                <a class="font-weight-bold"
-                                                                   href="{{asset('adminBoard\\uploadedFiles\\abouts\\'. $about->file)}}"
-                                                                   target="_blank">{!! __('general.download') !!}</a>
-
-                                                            </li>
-                                                        </ul>
-                                                    @endisset
-
-
-                                                </div>
-
-                                            </div>
-                                        </div>
-
-                                    </div>
-                                </div>
-                            </section>
-                            <!-- about-style-three end -->
 
                         </div>
                     </div>
                 </section>
+
             @elseif (!isset($about))
                 <!-- Page Title -->
                 <section class="page-title"
