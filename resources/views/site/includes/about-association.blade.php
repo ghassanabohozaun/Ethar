033c3ab73431d524c9c0d2c588894a7a747aec98
ghassan <ghassan@digitalorder.net>
2023-02-27 16:12:23 +0200

    sliders

--- resources/views/site/includes/about-association.blade.php
+++ resources/views/site/includes/about-association.blade.php
@@ -1,4 +1,4 @@
-<section class="about-section sec-pad" style="margin-top: 80px">
+<section class="about-section sec-pad mt-8">
     <div class="pattern-layer"
          style="background-image: url({!! asset('/site/assets/images/shape/shape-5.png') !!});"></div>
     <div class="auto-container">
