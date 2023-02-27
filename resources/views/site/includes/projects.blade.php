f081af90e3802635f8e20c788db85448c65d5219
ghassan <ghassan@digitalorder.net>
2023-02-27 16:06:51 +0200

    sliders

--- resources/views/site/includes/projects.blade.php
+++ resources/views/site/includes/projects.blade.php
@@ -1,47 +1,50 @@
 <!-- case-page-section -->
-<section class="case-page-section">
-    <div class="auto-container">
-        <div class="sec-title style-two centred">
-            <span class="top-text">{!! __('index.about_our_projects') !!}</span>
-            <h2>{!! __('index.our_projects') !!}</h2>
-            <p>
-                {!! fixedTexts()->{'project_details_'.Lang()} !!}
-            </p>
-        </div>
-        <div class="row clearfix">
 
-            @forelse($lastProjects as $project)
-                <div class="col-lg-3 col-md-6 col-sm-12 case-block">
-                    <div class="case-block-one">
-                        <div class="inner-box">
-                            <figure class="image-box">
-                                <img src="{{asset('adminBoard/uploadedImages/projects/'.$project->photo)}}"
-                                     alt="{!!  $project->{'title_'.Lang()} !!}">
-                            </figure>
-                            <div class="lower-content">
-                                <div class="shape"
-                                     style="background-image: url('{!! asset('/site/assets/images/shape/shape-11.png') !!}');">
-                                </div>
-                                <div class="inner">
-                                    <div class="text">
-                                        <div class="category">
-                                            <a href="{!!$project->type =='current'  ? route('projects','current') :route('projects','previous') !!}">
-                                                # {!! $project->type =='current'  ? __('index.current_project'):  __('index.previous_project')!!}
-                                            </a>
+@if($lastProjects)
+    <section class="case-page-section">
+        <div class="auto-container">
+            <div class="sec-title style-two centred">
+                <span class="top-text">{!! __('index.about_our_projects') !!}</span>
+                <h2>{!! __('index.our_projects') !!}</h2>
+                <p>
+                    {!! fixedTexts()->{'project_details_'.Lang()} !!}
+                </p>
+            </div>
+            <div class="row clearfix">
+
+                @foreach($lastProjects as $project)
+                    <div class="col-lg-3 col-md-6 col-sm-12 case-block">
+                        <div class="case-block-one">
+                            <div class="inner-box">
+                                <figure class="image-box">
+                                    <img src="{{asset('adminBoard/uploadedImages/projects/'.$project->photo)}}"
+                                         alt="{!!  $project->{'title_'.Lang()} !!}">
+                                </figure>
+                                <div class="lower-content">
+                                    <div class="shape"
+                                         style="background-image: url('{!! asset('/site/assets/images/shape/shape-11.png') !!}');">
+                                    </div>
+                                    <div class="inner">
+                                        <div class="text">
+                                            <div class="category">
+                                                <a href="{!!$project->type =='current'  ? route('projects','current') :route('projects','previous') !!}">
+                                                    # {!! $project->type =='current'  ? __('index.current_project'):  __('index.previous_project')!!}
+                                                </a>
+                                            </div>
+                                            <h3>
+                                                <a href="{!! route('project-details',slug($project->{'title_'.Lang()}) )!!}">{!!  $project->{'title_'.Lang()} !!}</a>
+                                            </h3>
                                         </div>
-                                        <h3>
-                                            <a href="{!! route('project-details',slug($project->{'title_'.Lang()}) )!!}">{!!  $project->{'title_'.Lang()} !!}</a>
-                                        </h3>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                </div>
-            @empty
-            @endforelse
 
+                @endforeach
+
+            </div>
         </div>
-    </div>
-</section>
+    </section>
+@endif
 <!-- case-page-section end -->
