a6f815a1c04079ceb4aba5543baed2834ec29e7e
ghassan <ghassan@digitalorder.net>
2023-02-28 11:51:19 +0200

    updates

--- resources/views/site/includes/founders.blade.php
+++ resources/views/site/includes/founders.blade.php
@@ -1,57 +1,55 @@
-<!-- team-section -->
-sdjfsdk jkddldkl'k lk
-kpks fk fsdf
-<section class="team-section my-team-section centred"
-         style="background-image: url(http://127.0.0.1:8000/site/assets/images/shape/shape-24.png);">
-    <div class="pattern-layer "></div>
-    <div class="fluid-container">
-        <div class="sec-title centred">
-            <span class="top-text">{!! __('index.meet_our_founders') !!}</span>
-            <h2>{!! fixedTexts()->{'founders_title_'.Lang()} !!}</h2>
-        </div>
-        <div class="five-item-carousel owl-carousel owl-theme owl-nav-none">
+@if(!$founders->isEmpty())
+    <section class="team-section my-team-section centred"
+             style="background-image: url(http://127.0.0.1:8000/site/assets/images/shape/shape-24.png);">
+        <div class="pattern-layer "></div>
+        <div class="fluid-container">
+            <div class="sec-title centred">
+                <span class="top-text">{!! __('index.meet_our_founders') !!}</span>
+                <h2>{!! fixedTexts()->{'founders_title_'.Lang()} !!}</h2>
+            </div>
+            <div class="five-item-carousel owl-carousel owl-theme owl-nav-none">
 
-            @forelse($founders as $founder)
-                <div class="team-block-one">
-                    <div class="inner-box">
-                        <figure class="image-box">
-                            <img src="{{asset('adminBoard/uploadedImages/teams/'.$founder->photo)}}"
-                                 alt="{!! $founder->{'name_'.Lang()} !!}">
-                        </figure>
-                        <div class="content-box">
-                            <div class="info">
-                                <span class="designation">{!! __('index.founder') !!}</span>
-                                <h3>{!! $founder->{'name_'.Lang()} !!}</h3>
-                            </div>
-                            <figure class="thumb-box">
+                @forelse($founders as $founder)
+                    <div class="team-block-one">
+                        <div class="inner-box">
+                            <figure class="image-box">
                                 <img src="{{asset('adminBoard/uploadedImages/teams/'.$founder->photo)}}"
                                      alt="{!! $founder->{'name_'.Lang()} !!}">
                             </figure>
-                            <div class="text">
-                                <p>{!! $founder->{'description_'.Lang()} !!}</p>
+                            <div class="content-box">
+                                <div class="info">
+                                    <span class="designation">{!! __('index.founder') !!}</span>
+                                    <h3>{!! $founder->{'name_'.Lang()} !!}</h3>
+                                </div>
+                                <figure class="thumb-box">
+                                    <img src="{{asset('adminBoard/uploadedImages/teams/'.$founder->photo)}}"
+                                         alt="{!! $founder->{'name_'.Lang()} !!}">
+                                </figure>
+                                <div class="text">
+                                    <p>{!! $founder->{'description_'.Lang()} !!}</p>
+                                </div>
                             </div>
+                            <ul class="social-links clearfix">
+                                <li>
+                                    <a href="{!! $founder->facebook  ??  'javascript:void(0)' !!}" target="_blank">
+                                        <i class="fab fa-facebook-f"></i>
+                                    </a>
+                                </li>
+                                <li><a href="{!! $founder->twitter  ??  'javascript:void(0)' !!}" target="_blank">
+                                        <i class="fab fa-twitter"></i>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="{!! $founder->linkedIn  ??  'javascript:void(0)' !!}" target="_blank">
+                                        <i class="fab fa-linkedin-in"></i>
+                                    </a>
+                                </li>
+                            </ul>
                         </div>
-                        <ul class="social-links clearfix">
-                            <li>
-                                <a href="{!! $founder->facebook  ??  'javascript:void(0)' !!}" target="_blank">
-                                    <i class="fab fa-facebook-f"></i>
-                                </a>
-                            </li>
-                            <li><a href="{!! $founder->twitter  ??  'javascript:void(0)' !!}" target="_blank">
-                                    <i class="fab fa-twitter"></i>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="{!! $founder->linkedIn  ??  'javascript:void(0)' !!}" target="_blank">
-                                    <i class="fab fa-linkedin-in"></i>
-                                </a>
-                            </li>
-                        </ul>
                     </div>
-                </div>
-            @empty
-            @endforelse
+                @empty
+                @endforelse
+            </div>
         </div>
-    </div>
-</section>
-<!-- team-section end -->
+    </section>
+@endif
