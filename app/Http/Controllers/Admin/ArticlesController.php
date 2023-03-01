23f1cf84a99d7304cc72f61f71ea9c211342ed09
ghassan <ghassan@digitalorder.net>
2023-03-01 14:47:40 +0200

    update

--- app/Http/Controllers/Admin/ArticlesController.php
+++ app/Http/Controllers/Admin/ArticlesController.php
@@ -45,7 +45,7 @@ class ArticlesController extends Controller
         if ($request->hasFile('photo')) {
             $image = $request->file('photo');
             $destinationPath = public_path('adminBoard/uploadedImages/articles');
-            $photo_path = $this->saveResizeImage($image, $destinationPath, 370, 350);
+            $photo_path = $this->saveResizeImage($image, $destinationPath, 530, 300);
 
         } else {
             $photo_path = '';
@@ -105,11 +105,11 @@ class ArticlesController extends Controller
             if (!empty($article->photo)) {
                 $image = $request->file('photo');
                 $destinationPath = public_path('/adminBoard/uploadedImages/articles//');
-                $photo_path = $this->saveResizeImage($image, $destinationPath, 370, 350);
+                $photo_path = $this->saveResizeImage($image, $destinationPath, 530, 300);
             } else {
                 $image = $request->file('photo');
                 $destinationPath = public_path('/adminBoard/uploadedImages/articles//');
-                $photo_path = $this->saveResizeImage($image, $destinationPath, 370, 350);
+                $photo_path = $this->saveResizeImage($image, $destinationPath, 530, 300);
             }
         } else {
             if (!empty($article->photo)) {
