8787a2042d9e2c77d7b469738e28f6cfe6fba295
ghassan <ghassan@digitalorder.net>
2023-03-05 12:11:29 +0200

    update

--- database/seeds/FixedTextsSeeder.php
+++ database/seeds/FixedTextsSeeder.php
@@ -14,11 +14,9 @@ class FixedTextsSeeder extends Seeder
      */
     public function run()
     {
-
         $faker = Faker::create();
 
         FixedText::create([
-
             'project_details_ar'  =>$faker->sentence,
             'project_details_en' =>$faker->sentence,
             'about_association_title_ar' =>$faker->sentence,
@@ -37,20 +35,20 @@ class FixedTextsSeeder extends Seeder
             'testimonials_details_en' =>$faker->paragraph(10),
             'counter_icon_1' =>'charity-icon.jpg',
             'counter_number_1' =>$faker->numberBetween(0, 300),
-            'counter_name_1_ar' =>$faker->word,
-            'counter_name_1_en' =>$faker->word,
+            'counter_name_1_ar' =>'عداد رقم 1',
+            'counter_name_1_en' =>'Counter Number 1',
             'counter_icon_2' =>'charity-icon.jpg',
             'counter_number_2' =>$faker->numberBetween(0, 200),
-            'counter_name_2_ar' =>$faker->word,
-            'counter_name_2_en' =>$faker->word,
+            'counter_name_2_ar' =>'عداد رقم 1',
+            'counter_name_2_en' =>'Counter Number 2',
             'counter_icon_3' =>'charity-icon.jpg',
             'counter_number_3' =>$faker->numberBetween(0, 100),
-            'counter_name_3_ar' =>$faker->word,
-            'counter_name_3_en' =>$faker->word,
+            'counter_name_3_ar' =>'عداد رقم 3',
+            'counter_name_3_en' =>'Counter Number 3',
             'counter_icon_4' =>'charity-icon.jpg',
             'counter_number_4' =>$faker->numberBetween(0, 500),
-            'counter_name_4_ar' =>$faker->word,
-            'counter_name_4_en' =>$faker->word,
+            'counter_name_4_ar' =>'عداد رقم 4',
+            'counter_name_4_en' =>'Counter Number 4',
         ]);
     }
 }
