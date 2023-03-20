aa5285cf7042e7c2501434a21599f7d26ccfc9df
ghassan <ghassan@digitalorder.net>
2023-03-20 17:10:27 +0200

    update

--- public/site/assets/js/script.js
+++ public/site/assets/js/script.js
@@ -469,7 +469,8 @@
             nav: true,
             smartSpeed: 500,
             autoplay: 1000,
-            items: 5,
+            items: 1,
+            // items: 5,
             navText: ['<span class="fas fa-angle-left"></span>', '<span class="fas fa-angle-right"></span>'],
         });
     }
