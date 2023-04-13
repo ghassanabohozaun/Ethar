e94c61737aedaa49633768b6252ad91f61810d8d
ghassan <ghassan@digitalorder.net>
2023-04-13 13:04:59 +0300

    updates

--- resources/views/site/includes/partners-section.blade.php
+++ resources/views/site/includes/partners-section.blade.php
@@ -3,9 +3,9 @@
         <div class="clients-carousel owl-carousel owl-theme owl-dots-none owl-nav-none">
             @foreach($partners as $partner)
                 <figure class="clients-logo-box">
-                    <a href="{!! $partner->url ? $partner->url : '#'  !!}">
+                    <a href="{!! $partner->url ? $partner->url : '#'  !!}" target="_blank">
                         <img src="{{asset('adminBoard/uploadedImages/partners/'.$partner->photo)}}"
-                        alt="{!! $partner->{'name_'.Lang()} !!}">
+                             alt="{!! $partner->{'name_'.Lang()} !!}">
                     </a>
                 </figure>
             @endforeach
