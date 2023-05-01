26e6d7c298fc623d33f806d15f734c961e5a5de4
ghassan <ghassan@digitalorder.net>
2023-05-01 16:00:29 +0300

    updates

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -125,7 +125,7 @@
                 @if(!empty(setting()->site_logo))
                     <a href="#" class="brand-logo">
                         <img src="{{asset('adminBoard/uploadedImages/logos/'.setting()->site_logo)}}"
-                             style="width: 105px; height: 70px; border-radius: 80px">
+                             style="width: 100px; height: 65px; border-radius: 10px">
                     </a>
                 @else
                     LOGO
